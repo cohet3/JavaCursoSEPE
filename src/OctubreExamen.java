fb32fbabd8ee97fe0d7c7bb35e7a729152682e78
cohet3 <tauro.total90@gmail.com>
2023-10-20 15:35:06 +0200

    Ejercicio3 del examen

--- src/OctubreExamen.java
+++ src/OctubreExamen.java
@@ -3,5 +3,12 @@ public class OctubreExamen {
         //Examen octubre
         //1 . Crear un proyecto en intelegent
         //2. Crear el repositorio Git Hub
+        // 3. Hay que declarar 3 variables una entera con el nombre de edad= 18, otra doble con el nombre de sueldo=1200 y una char con sexo='m'
+        int edad = 18;
+        double sueldo = 1200;
+        char sexo = 'M';
+        System.out.println("edad= "+edad+" años");
+        System.out.println("sueldo= "+ sueldo +" €");
+        System.out.println("sexo "+sexo);
     }
 }

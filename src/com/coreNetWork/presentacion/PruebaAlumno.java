8748e6e7efe2cd957b0aa1d2639fb4d79f66d34d
cohet3 <tauro.total90@gmail.com>
2023-10-30 19:57:41 +0100

    final2 de la clase del dia 30_10_2023

--- src/com/coreNetWork/presentacion/PruebaAlumno.java
+++ src/com/coreNetWork/presentacion/PruebaAlumno.java
@@ -5,6 +5,10 @@ import com.coreNetWork.modelos.Alumno;
 public class PruebaAlumno {
     public static void main(String[] args) {
         Alumno a1=new Alumno("Pepito ", 3);
-
+        a1.asignarValores(8, 0);
+        a1.asignarValores(7,1);
+        a1.asignarValores(10,2);
+        System.out.println(a1.promedio());
+        System.out.println(a1.mostrar());
     }
 }

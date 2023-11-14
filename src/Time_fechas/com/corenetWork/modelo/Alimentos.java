89d8060997128d10b4d01d400f268380fea935f2
cohet3 <tauro.total90@gmail.com>
2023-11-14 18:11:04 +0100

    final de la clase 14-11-2023

---
+++ src/Time_fechas/com/corenetWork/modelo/Alimentos.java
@@ -0,0 +1,12 @@
+package Time_fechas.com.corenetWork.modelo;
+
+public class Alimentos extends Producto{
+    public Alimentos(int idProducto) {
+        super(idProducto);
+
+    }
+/* NO se puede sobreescribir un metodo final
+    public final double iva(){
+        return precio* 0.21;
+    }*/
+}
